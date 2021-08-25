e47bef570a5151421ec1b158257319413b82654f
Ekeleme <88858152+Ekeleme@users.noreply.github.com>
2021-08-25 11:21:21 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,23 @@
 ﻿using System;
 
-namespace Multiplication
+namespace Subtraction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            //Takes three numbers and Multiplies them. 
-            Console.WriteLine("Hi, This application will Multiply the three numbers you'll be asked to provide.");
-            
+            //Takes two numbers and Subtracts them from each other. 
+            Console.WriteLine("Hi, This application will Subtract the two numbers you'll be asked to provide.");
+
             Console.Write("Please Enter Your First Number... ");
             int firstInput = int.Parse(Console.ReadLine());
             
             Console.Write("Please Enter Your Second Number... ");
             int secondInput = int.Parse(Console.ReadLine());
-            
-            Console.Write("Please Enter Your Third Number... ");
-            int thirdInput = int.Parse(Console.ReadLine());
 
-            int result = firstInput * secondInput * thirdInput;
+            int result = firstInput - secondInput;
 
-            Console.WriteLine("Your Answer is " + result); 
+            Console.WriteLine("Your Answer is " + result);
         }
     }
 }
