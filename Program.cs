eebfd484592f0761e8389989799d58f84ade125e
Ekeleme <88858152+Ekeleme@users.noreply.github.com>
2021-08-25 11:25:08 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 
-namespace Subtraction
+namespace Division
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Takes two numbers and Subtracts them from each other. 
-            Console.WriteLine("Hi, This application will Subtract the two numbers you'll be asked to provide.");
-
+            //Takes two numbers and Divides them. 
+            Console.WriteLine("Hi, This application will Divide the two numbers you'll be asked to provide.");
+            
             Console.Write("Please Enter Your First Number... ");
             int firstInput = int.Parse(Console.ReadLine());
             
             Console.Write("Please Enter Your Second Number... ");
             int secondInput = int.Parse(Console.ReadLine());
+            
 
-            int result = firstInput - secondInput;
+            int result = firstInput / secondInput;
 
             Console.WriteLine("Your Answer is " + result);
         }
