f20dd9458897224c5ae5204379c456560cb1d790
Ekeleme <88858152+Ekeleme@users.noreply.github.com>
2021-08-25 11:09:32 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Takes three numbers and Multiplies them. 
+            Console.WriteLine("Hi, This application will Multiply the three numbers you'll be asked to provide.");
+            
+            Console.Write("Please Enter Your First Number... ");
+            int firstInput = int.Parse(Console.ReadLine());
+            
+            Console.Write("Please Enter Your Second Number... ");
+            int secondInput = int.Parse(Console.ReadLine());
+            
+            Console.Write("Please Enter Your Third Number... ");
+            int thirdInput = int.Parse(Console.ReadLine());
+
+            int result = firstInput * secondInput * thirdInput;
+
+            Console.WriteLine("Your Answer is " + result); 
+        }
+    }
+}
